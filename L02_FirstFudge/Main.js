289c942cf3a6154a0c054384b90a704729c1e28d
SaschaK13 <sascha.patrick.koenninger@hs-furtwangen.de>
2019-10-17 15:17:49 +0200

    3 nodes

--- L02_FirstFudge/Main.js
+++ L02_FirstFudge/Main.js
@@ -7,26 +7,43 @@ var L02_FirstFudge;
         const canvas = document.querySelector("canvas");
         fudge.RenderManager.initialize();
         fudge.Debug.log(canvas);
+        /** NODES **/
         let parentNode = new fudge.Node("Parent");
-        let node = new fudge.Node("Cube");
-        let pyramidNode = new fudge.Node("Pyramid");
+        let node = new fudge.Node("Quad");
+        let nodePlayer = new fudge.Node("PlayerQuad");
+        let nodePlayer2 = new fudge.Node("Player2Quad");
         parentNode.appendChild(node);
-        parentNode.appendChild(pyramidNode);
-        let mesh = new fudge.MeshCube();
+        parentNode.appendChild(nodePlayer);
+        parentNode.appendChild(nodePlayer2);
+        let mesh = new fudge.MeshQuad();
+        let coat = new fudge.CoatColored(new fudge.Color(1, 0, 1, 1));
+        let mtrHotPink = new fudge.Material("HotPink", fudge.ShaderUniColor, coat);
+        let coat2 = new fudge.CoatColored(new fudge.Color(1, 1, 1, 1));
+        let mtrSolidWhite = new fudge.Material("SolidWhite", fudge.ShaderUniColor, coat2);
+        /** BALL **/
         let cmpMesh = new fudge.ComponentMesh(mesh);
         cmpMesh.pivot.translate(new fudge.Vector3(0, 0, 0));
+        cmpMesh.pivot.scale(new fudge.Vector3(0.5, 0.5, 0.5));
         node.addComponent(cmpMesh);
-        let pyramidMesh = new fudge.MeshPyramid();
-        let pyramidCmpMesh = new fudge.ComponentMesh(pyramidMesh);
-        pyramidCmpMesh.pivot.translate(new fudge.Vector3(1, 1, 3));
-        pyramidNode.addComponent(pyramidCmpMesh);
-        let coat = new fudge.CoatColored(new fudge.Color(1, 0, 1, 1));
-        let mtrHotPink = new fudge.Material("SolidWhite", fudge.ShaderUniColor, coat);
-        let cmpColor = new fudge.ComponentMaterial(mtrHotPink);
-        node.addComponent(cmpColor);
-        //pyramidNode.addComponent(cmpColor); 
+        let cmpMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMaterial);
+        /** PLAYER **/
+        let cmpMeshPlayer = new fudge.ComponentMesh(mesh);
+        cmpMeshPlayer.pivot.translate(new fudge.Vector3(2, 0, 0));
+        cmpMeshPlayer.pivot.scale(new fudge.Vector3(1, 2, 1));
+        nodePlayer.addComponent(cmpMeshPlayer);
+        let cmpMaterialPlayer = new fudge.ComponentMaterial(mtrHotPink);
+        nodePlayer.addComponent(cmpMaterialPlayer);
+        /** PLAYER2 **/
+        let cmpMeshPlayer2 = new fudge.ComponentMesh(mesh);
+        cmpMeshPlayer2.pivot.translate(new fudge.Vector3(-2, 0, 0));
+        cmpMeshPlayer2.pivot.scale(new fudge.Vector3(1, 2, 1));
+        nodePlayer2.addComponent(cmpMeshPlayer2);
+        let cmpMaterialPlayer2 = new fudge.ComponentMaterial(mtrHotPink);
+        nodePlayer2.addComponent(cmpMaterialPlayer2);
+        /** CAMERA **/
         let cmpCamera = new fudge.ComponentCamera();
-        cmpCamera.pivot.translate(new fudge.Vector3(3, 3, 10));
+        cmpCamera.pivot.translate(new fudge.Vector3(0, 0, 6));
         cmpCamera.pivot.lookAt(new fudge.Vector3(0, 0, 0));
         L02_FirstFudge.viewport = new fudge.Viewport();
         L02_FirstFudge.viewport.initialize("Viewport", parentNode, cmpCamera, canvas);
--- L02_FirstFudge/Main.ts
+++ L02_FirstFudge/Main.ts
@@ -9,30 +9,60 @@ namespace L02_FirstFudge {
         fudge.RenderManager.initialize();
         fudge.Debug.log(canvas);
 
+        /** NODES **/
         let parentNode: fudge.Node = new fudge.Node("Parent");
-        let node: fudge.Node = new fudge.Node("Cube");
-        let pyramidNode: fudge.Node = new fudge.Node("Pyramid");
-        parentNode.appendChild(node);
-        parentNode.appendChild(pyramidNode);
+        let node: fudge.Node = new fudge.Node("Quad");
+        let nodePlayer: fudge.Node = new fudge.Node("PlayerQuad");
+        let nodePlayer2: fudge.Node = new fudge.Node("Player2Quad");
 
-        let mesh: fudge.MeshCube = new fudge.MeshCube();
-        let cmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
-        cmpMesh.pivot.translate(new fudge.Vector3(0, 0, 0));
-        node.addComponent(cmpMesh);
- 
-        let pyramidMesh: fudge.MeshPyramid = new fudge.MeshPyramid();
-        let pyramidCmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(pyramidMesh);
-        pyramidCmpMesh.pivot.translate(new fudge.Vector3(1, 1, 3));
-        pyramidNode.addComponent(pyramidCmpMesh); 
+        parentNode.appendChild(node);
+        parentNode.appendChild(nodePlayer);
+        parentNode.appendChild(nodePlayer2);
+
+        let mesh: fudge.MeshQuad = new fudge.MeshQuad();
 
         let coat: fudge.CoatColored = new fudge.CoatColored(new fudge.Color(1, 0, 1, 1));
-        let mtrHotPink: fudge.Material = new fudge.Material("SolidWhite", fudge.ShaderUniColor, coat);
-        let cmpColor: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrHotPink);
-        node.addComponent(cmpColor);
-        //pyramidNode.addComponent(cmpColor); 
- 
+        let mtrHotPink: fudge.Material = new fudge.Material("HotPink", fudge.ShaderUniColor, coat);
+
+        let coat2: fudge.CoatColored = new fudge.CoatColored(new fudge.Color(1, 1, 1, 1));
+        let mtrSolidWhite: fudge.Material = new fudge.Material("SolidWhite", fudge.ShaderUniColor, coat2);
+
+        /** BALL **/
+        let cmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
+        cmpMesh.pivot.translate(new fudge.Vector3(0, 0, 0));
+        cmpMesh.pivot.scale(new fudge.Vector3(0.5, 0.5, 0.5));
+
+        node.addComponent(cmpMesh);
+
+        let cmpMaterial: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
+
+        node.addComponent(cmpMaterial);
+
+        /** PLAYER **/
+        let cmpMeshPlayer: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
+        cmpMeshPlayer.pivot.translate(new fudge.Vector3(2, 0, 0));
+        cmpMeshPlayer.pivot.scale(new fudge.Vector3(1, 2, 1));
+
+        nodePlayer.addComponent(cmpMeshPlayer);
+
+        let cmpMaterialPlayer: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrHotPink);
+
+        nodePlayer.addComponent(cmpMaterialPlayer);
+
+        /** PLAYER2 **/
+        let cmpMeshPlayer2: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
+        cmpMeshPlayer2.pivot.translate(new fudge.Vector3(-2, 0, 0));
+        cmpMeshPlayer2.pivot.scale(new fudge.Vector3(1, 2, 1));
+  
+        nodePlayer2.addComponent(cmpMeshPlayer2);
+
+        let cmpMaterialPlayer2: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrHotPink);
+
+        nodePlayer2.addComponent(cmpMaterialPlayer2);
+
+        /** CAMERA **/ 
         let cmpCamera: fudge.ComponentCamera = new fudge.ComponentCamera();
-        cmpCamera.pivot.translate(new fudge.Vector3(3, 3, 10));
+        cmpCamera.pivot.translate(new fudge.Vector3(0, 0, 6));
         cmpCamera.pivot.lookAt(new fudge.Vector3(0, 0, 0));
 
         viewport = new fudge.Viewport();
