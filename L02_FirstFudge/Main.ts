5be8cedfe06aedfb5cd2a8c11395635ecb25fd74
SaschaSq <koenninger@sq.de>
2019-10-17 12:57:57 +0200

    added a new node

--- L02_FirstFudge/Main.ts
+++ L02_FirstFudge/Main.ts
@@ -9,23 +9,34 @@ namespace L02_FirstFudge {
         fudge.RenderManager.initialize();
         fudge.Debug.log(canvas);
 
-        let node: fudge.Node = new fudge.Node("Quad");
+        let parentNode: fudge.Node = new fudge.Node("Parent");
+        let node: fudge.Node = new fudge.Node("Cube");
+        let pyramidNode: fudge.Node = new fudge.Node("Pyramid");
+        parentNode.appendChild(node);
+        parentNode.appendChild(pyramidNode);
 
-        let mesh: fudge.MeshQuad = new fudge.MeshQuad();
+        let mesh: fudge.MeshCube = new fudge.MeshCube();
         let cmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
+        cmpMesh.pivot.translate(new fudge.Vector3(0, 0, 0));
         node.addComponent(cmpMesh);
+ 
+        let pyramidMesh: fudge.MeshPyramid = new fudge.MeshPyramid();
+        let pyramidCmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(pyramidMesh);
+        pyramidCmpMesh.pivot.translate(new fudge.Vector3(1, 1, 3));
+        pyramidNode.addComponent(pyramidCmpMesh); 
 
-        let coat: fudge.CoatColored = new fudge.CoatColored(new fudge.Color(0, 0, 1, 1));
+        let coat: fudge.CoatColored = new fudge.CoatColored(new fudge.Color(1, 0, 1, 1));
         let mtrHotPink: fudge.Material = new fudge.Material("SolidWhite", fudge.ShaderUniColor, coat);
         let cmpColor: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrHotPink);
         node.addComponent(cmpColor);
-
+        //pyramidNode.addComponent(cmpColor); 
+ 
         let cmpCamera: fudge.ComponentCamera = new fudge.ComponentCamera();
-        cmpCamera.pivot.translate(new fudge.Vector3(0, 0, 3));
+        cmpCamera.pivot.translate(new fudge.Vector3(3, 3, 10));
         cmpCamera.pivot.lookAt(new fudge.Vector3(0, 0, 0));
 
         viewport = new fudge.Viewport();
-        viewport.initialize("Viewport", node, cmpCamera, canvas);
+        viewport.initialize("Viewport", parentNode, cmpCamera, canvas);
         fudge.Debug.log(viewport);
 
         viewport.draw();
